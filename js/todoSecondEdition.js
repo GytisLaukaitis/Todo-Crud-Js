60ce04d8e51ffdacecae60f4aef43b153e9c207e
gytislaukaitisi <63237716+gytislaukaitisi@users.noreply.github.com>
2020-05-14 13:04:32 +0300

    todo

--- js/todoSecondEdition.js
+++ js/todoSecondEdition.js
@@ -1,4 +1,6 @@
 "use strict";
+let todo_id = 0;
+let todo_list = [];
 
 const DOMcontainer = document.querySelector('.container');
 
@@ -7,10 +9,13 @@ const BTNremoveAll = DOMglobals.querySelector('.action.remove');
 
 const DOMform = DOMcontainer.querySelector('.form');
 const DOMtaskTextarea = DOMform.querySelector('textarea[name="task"]');
+const DOMswitchStatus = DOMform.querySelector('.switch');
 const DOMdeadlineInput = DOMform.querySelector('input[name="deadline"]');
 const DOMformActions = DOMform.querySelector('.actions');
 const DOMformAdd = DOMformActions.querySelector('.btn.add');
 const DOMformClear = DOMformActions.querySelector('.btn.clear');
+const DOMformSave = DOMformActions.querySelector('.btn.save');
+const DOMformCancel = DOMformActions.querySelector('.btn.cancel');
 
 let DOMitems = null;
 
@@ -21,18 +26,43 @@ function renderList( list ) {
 }
 
 function renderTodoItem( data ) {
+    const id = 'todo_'+data.id;
     const HTML = `
-        <div class="item">
+        <div class="item" id="${id}">
             <div class="status ${data.status}"></div>
             <p class="description">${data.description}</p>
             <div class="deadline">${data.deadline}</div>
             <div class="actions">
                 <div class="action remove">Remove</div>
+                <div class="action edit">Edit</div>
             </div>
         </div>`;
     
     DOMcontainer.insertAdjacentHTML('beforeend', HTML);
     DOMitems = DOMcontainer.querySelectorAll('.item');
+
+    const item = DOMcontainer.querySelector('#'+id);
+    
+    // registruojame event listener'i
+    item.querySelector('.action.remove')
+        .addEventListener('click', () => {
+            let currentlyAddedItemIndex = 0;
+
+            // randu kelintas siuo metu sarase yra norimas mano TODO item
+            for ( let i=0; i<DOMitems.length; i++ ) {
+                if ( DOMitems[i].id === id ) {
+                    currentlyAddedItemIndex = i;
+                    break;
+                }
+            }
+
+            removeTodo( currentlyAddedItemIndex );
+        });
+
+    item.querySelector('.action.edit')
+        .addEventListener('click', () => {
+            DOMform.classList.add('editing');
+        });
     return;
 }
 
@@ -66,7 +96,7 @@ function formatedDate( deltaTime = 0 ) {
 }
 
 function removeAllTodos() {
-    for ( let i=4-1; i>=0; i-- ) {
+    for ( let i=DOMitems.length-1; i>=0; i-- ) {
         removeTodo(i);
     }
 }
@@ -83,9 +113,70 @@ function removeTodo( todoIndex ) {
             leftTodos.push( todo_list[i] );
         }
     }
+    
     todo_list = leftTodos;
+    updateMemory();
     return;
 }
+
+function createNewTodo() {
+    let newTodo = {
+        id: todo_id,
+        description: DOMtaskTextarea.value.trim(),
+        created_on: formatedDate(),
+        deadline: DOMdeadlineInput.value.trim(),
+        status: 'todo'
+    };
+
+    if ( newTodo.description.length === 0 ) {
+        return alert('ERROR: tuscias aprasymas');
+    }
+    
+    if ( newTodo.deadline.length > 0 &&
+        (new Date(newTodo.deadline)).toString() === 'Invalid Date' ) {
+        return alert('ERROR: nevalidus deadline');
+    }
+    
+    todo_list.push( newTodo );
+    renderTodoItem( newTodo );
+    todo_id++;
+    updateMemory();
+}
+
+function updateSwitch( event ) {
+    const value = event.target.dataset.option;
+    event.target.parentElement.setAttribute('data-selected', value);
+}
+
+/*******************************
+    MEMORY MANAGEMENT
+*******************************/
+
+function memoryManagement() {
+    if ( localStorage.getItem('todo_id') ) {
+        // jei yra, tai is localStorage istraukiu esama reiksme ir ja priskiriu todo_id
+        todo_id = JSON.parse( localStorage.getItem('todo_id') );
+    } else {
+        // jei localStorage nera todo_id, tai ji sukuriu ir priskiriu reiksme 0
+        localStorage.setItem('todo_id', JSON.stringify(todo_id));
+    }
+
+    
+    if ( localStorage.getItem('todo_list') ) {
+        // jei yra, tai is localStorage istraukiu esama reiksme ir ja priskiriu todo_list
+        todo_list = JSON.parse( localStorage.getItem('todo_list') );
+    } else {
+        // jei localStorage nera todo_list, tai ji sukuriu ir priskiriu reiksme []
+        localStorage.setItem('todo_list', JSON.stringify(todo_list));
+    }
+}
+
+function updateMemory() {
+    localStorage.setItem('todo_id', JSON.stringify(todo_id));
+    localStorage.setItem('todo_list', JSON.stringify(todo_list));
+}
+
+memoryManagement();
 
 /*******************************
     GENERATE CONTENT
@@ -99,3 +190,11 @@ DOMdeadlineInput.value = formatedDate( 86400000 );
 *******************************/
 
 BTNremoveAll.addEventListener('click', removeAllTodos);
+
+DOMformAdd.addEventListener('click', createNewTodo);
+
+DOMswitchStatus.addEventListener('click', updateSwitch);
+
+DOMformCancel.addEventListener('click', () => {
+    DOMform.classList.remove('editing');
+})
